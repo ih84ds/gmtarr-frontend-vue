73c8cf60b4e6be5b9b2a1f7851d26752f14595d0
Jeremy Parrish <jeremyp@rrish.org>
2018-05-08 20:53:19 -0500

    moves prod path to /app

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: '"production"',
   API_BASE_URL: '"https://roundrobin.gmtatennis.org/api/"',
   WA_OAUTH_LOGIN_URL: "'https://www.gmtatennis.org/sys/login/OAuthLogin'",
-  ROUTER_BASE: "'/vue/'",
+  ROUTER_BASE: "'/app/'",
 }
