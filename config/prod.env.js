ee147086f8412255fe5c47ec03c9414d096b920e
Jeremy Parrish <jeremyp@rrish.org>
2019-04-07 22:41:25 -0500

    updates for firebase hosting and lambda backend

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_BASE_URL: '"https://roundrobin.gmtatennis.org/api/"',
+  API_BASE_URL: '"https://gmtarr-api.rrish.org/api/"',
   WA_OAUTH_LOGIN_URL: "'https://www.gmtatennis.org/sys/login/OAuthLogin'",
-  ROUTER_BASE: "'/app/'",
+  ROUTER_BASE: "'/'",
 }
